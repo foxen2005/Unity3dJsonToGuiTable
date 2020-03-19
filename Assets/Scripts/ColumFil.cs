4aa24f922776c44511b680e1434ace92e302278a
Jean C. Baptista <foxen2005@gmail.com>
2020-03-19 00:00:20 -0300

    Upload

---
+++ Assets/Scripts/ColumFil.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using SimpleJSON;
+
+public class ColumFil : MonoBehaviour
+{
+    [SerializeField]
+    public string TextFile, textColum;
+    public string[] DataList, TitleList;
+    public int ListSize,TitleSize;
+    public int ListPosition, TitlePosition;
+
+
+    public Text titleMain, ColumValue;
+    public int NameValue;
+
+    public string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "JsonChallenge.json");
+
+
+    void Start()
+    {
+
+
+        // TextFile = File.ReadAllText("Assets/StreamingAssets/JsonChallenge.json");
+        TextFile = File.ReadAllText(filePath);
+
+        StartCoroutine(LectorColumna());
+
+        DataList = new string[ListSize];
+        TitleList = new string[TitleSize];
+
+        StopAllCoroutines();
+    }
+
+     void FixedUpdate()
+    {
+        NameValue = int.Parse(gameObject.name);
+
+
+        StartCoroutine(LectorColumna());
+
+        // StopAllCoroutines();
+
+        titleMain.text = TitleList[NameValue];
+        ColumValue.text = textColum;
+    }
+
+    public IEnumerator LectorColumna()
+    {
+        var DATA = JSON.Parse(TextFile);
+       
+
+        ListSize = DATA["Data"].Count;
+        TitleSize = DATA["ColumnHeaders"].Count;
+
+        if (TitlePosition < TitleSize)
+        {
+            TitleList[TitlePosition] = DATA["ColumnHeaders"][TitlePosition].Value;//titulos
+            TitlePosition++;
+        }
+
+        if (ListPosition < ListSize)
+        {
+
+            DataList[ListPosition] = DATA["Data"][ListPosition][TitleList[NameValue]].Value; //lista
+            // textColum += DataList[ListPosition] = DATA["Data"][ListPosition][TitleList[NameValue]].Value +"\n"; //lista
+
+            textColum += DataList[ListPosition] + "\n";
+            ListPosition++;
+        }
+        
+        
+
+       
+        yield return null;
+    }
+
+}
---
+++ Assets/Scripts/Reader.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using SimpleJSON;
+
+public class Reader : MonoBehaviour
+{
+
+
+    ///
+    public int numList, sizeList, sizeColum, numColum;
+    
+    public string[]  ID, Name_s, Role_s, Nick_s;
+    public string[] ColumnHeaders;
+
+    public static string[] ColHeaders;
+
+    public string FileTitle;
+    public string TextFile;
+
+    //GUI
+    public Text[] LineTitle;
+    public Text fileTitle_tx, columTest1, columTest2, columTest3, columTest4, fila1, fila2, fila3, fila4;
+
+    public string FilaData_tx;
+    public string[] columnasData_tx;
+
+    public int runText ,instaserNum = 0;
+
+    public GameObject ColumnaMaster;
+    GameObject[] InstaColumnas;
+    public Transform ColumBlock;
+
+    public string filePath = System.IO.Path.Combine(Application.streamingAssetsPath, "JsonChallenge.json");
+
+    void Start()
+    {
+
+        //TextFile = File.ReadAllText("Assets/StreamingAssets/JsonChallenge.json");
+        TextFile = File.ReadAllText(filePath);
+
+        StartCoroutine(Lector());
+        InstaColumnas = new GameObject[sizeColum];
+    }
+
+
+
+    void FixedUpdate()
+       {
+
+        StartCoroutine(Lector());
+       
+        fileTitle_tx.text = FileTitle;
+
+        if (instaserNum < sizeColum)
+        {
+            InstaColumnas[instaserNum] = (GameObject)Instantiate(ColumnaMaster, ColumBlock);
+            InstaColumnas[instaserNum].name = "" + instaserNum;
+            instaserNum++;
+        }
+
+        StopAllCoroutines();
+
+
+    }
+
+    public IEnumerator Lector()
+    {
+        
+        var DATA = JSON.Parse(TextFile);
+
+        FileTitle = DATA["Title"].Value;
+        sizeColum = DATA["ColumnHeaders"].Count;
+        sizeList = DATA["Data"].Count;
+
+       
+        yield return null;
+    }
+
+
+
+   
+   
+}
+
+
